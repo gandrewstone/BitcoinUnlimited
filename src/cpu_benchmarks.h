4ba4449b129ef8ba5ca376317d1043e17c452347
Greg Griffith <cryptounitedteam@gmail.com>
2017-11-19 01:23:30 -0800

    add serialization to a CStat and add a CStat benchmark class for a base to store recorded stats against

--- src/cpu_benchmarks.h
+++ src/cpu_benchmarks.h
@@ -2,9 +2,44 @@
 #define CPU_BENCHMARKS_H
 
 #include <stdint.h>
-#include "sync.h"
+#include "stat.h"
 
 extern CCriticalSection cs_block_benchmarks;
+
+
+
+/* used to store a Stat on disk for later reading, contains additional data on top of a Stat */
+class CBenchmarkStat
+{
+public:
+    std::string benchmarkName;
+    CStatMap mapStats;
+    int nStartHeight;
+    int nEndHeight;
+    int64_t nStartTime;
+    int64_t nEndTime;
+    int nBlockCount;
+    int64_t nRunTime;
+
+public:
+    CBenchmarkStat()
+    {
+        setNull();
+    }
+
+    void setNull()
+    {
+        benchmarkName.clear();
+        mapStats.clear();
+        nStartHeight = 0;
+        nEndHeight = 0;
+        nStartTime = 0;
+        nEndTime = 0;
+        nBlockCount = 0;
+        nRunTime = 0;
+    }
+}
+
 
 // additional info about all timers in cpu_benchmarks.cpp
 
